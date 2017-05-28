5a0677cc4d2a5e48236d513649050141c1c5d823
Jahazielmr <Jahazielmr97@gmail.com>
2017-05-27 19:43:14 -0600

    Creacion de Clases

---
+++ Persona.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef PERSONA_H
+#define PERSONA_H
+
+class Persona{
+    protected:
+        string Nombre;
+        string Username;
+        string Password;
+        int Edad;
+        string Carrera;
+    public:
+        Persona(string,string,string,int,string);
+        Persona();
+        string getNombre();
+        void setNombre(string);
+
+        string getUsername();
+        void setUsername(string);
+
+        string getPassword();
+        void setPassword(string);
+
+        int getEdad();
+        void setEdad(int);
+
+        string getCarrera();
+        void setCarrera(string);
+
+};
+#endif
