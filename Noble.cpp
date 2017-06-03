7aa7abacb6194d9e089abd1aba3c7c61a70f35d2
David <david_elvir@unitec.edu>
2017-06-03 17:03:27 -0600

    Noble

---
+++ Noble.cpp
@@ -0,0 +1,14 @@
+#include "Noble.h"
+#include "Humano.h"
+
+
+Noble::Noble(){
+  this->Vida=200;
+  this->Defensa=100;
+  this->Ataque=180;
+  this->Stamina = true;
+  this->Dinero=5000;
+  this->Nivel=1;
+  this->Experiencia=0;
+  this->Bolas=0;
+}
