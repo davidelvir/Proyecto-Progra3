acf0fade685c9a5ad28d9c8d8284de133d9673dc
David <david_elvir@unitec.edu>
2017-06-03 16:37:44 -0600

    m

--- Guerrero.cpp
+++ Guerrero.cpp
@@ -2,9 +2,9 @@
 
 Humano::Humano(string Nombre, double Vida, double Defensa, double Ataque,string Reputacion,int Nivel,int Experiencia,int Bolas,string estiloCabello, string Color, double Dinero){
     this->Nombre=Nombre;
-    this->Vida=Vida;
-    this->Defensa=Defensa;
-    this->Ataque=Ataque;
+    this->Vida=300;
+    this->Defensa=150;
+    this->Ataque=200;
     this->Reputacion=Reputacion;
     this->Nivel=Nivel;
     this->Experiencia=Experiencia;
@@ -12,8 +12,7 @@ Humano::Humano(string Nombre, double Vida, double Defensa, double Ataque,string 
     this->estiloCabello=estiloCabello;
     this->Stamina=true;
     this->Color=Color;
-    this->Dinero=Dinero;
-    
+    this->Dinero=0;
 }
 
 Humano::Humano(){
---
+++ Guerrero.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Humano.h"
+
+using namespace std;
+
+#ifndef Guerrero_H
+#define Guerrero_H
+
+class Guerrero:public Humano{
+
+    protected:
+
+
+    public:
+        
+    	Guerrero(string, double,double,double,string,int,int,int,string, string, double);
+        Guerrero();
+        
+};
+#endif
