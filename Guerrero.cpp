5df185ec9ccd406c1a6450fd904017482e261c44
Jahazielmr <Jahazielmr97@gmail.com>
2017-06-03 16:47:09 -0600

    JUVE

--- Guerrero.cpp
+++ Guerrero.cpp
@@ -1,10 +1,10 @@
-#include "Humano.h"
+#inclu	de "Humano			.				h"	
 
 Humano::Humano(string Nombre, double Vida, double Defensa, double Ataque,string Reputacion,int Nivel,int Experiencia,int Bolas,string estiloCabello, string Color, double Dinero){
     this->Nombre=Nombre;
-    this->Vida=300;
-    this->Defensa=150;
-    this->Ataque=200;
+    this->Vida=Vida;
+    this->Defensa=Defensa;
+    this->Ataque=Ataque;
     this->Reputacion=Reputacion;
     this->Nivel=Nivel;
     this->Experiencia=Experiencia;
@@ -12,10 +12,21 @@ Humano::Humano(string Nombre, double Vida, double Defensa, double Ataque,string 
     this->estiloCabello=estiloCabello;
     this->Stamina=true;
     this->Color=Color;
-    this->Dinero=0;
+    this->Dinero=Dinero;
 }
 
 Humano::Humano(){
+
+	this->Vida=300;
+	this->Defensa=150;
+	this->Ataque=200;
+	this->Stamina=true;
+	this->Dinero=0;
+	this->Nivel=1;
+	this->Experiencia=0;
+	this->Reputacion=0;
+	this->Bolas=0;
+
 }
 
 void Humano::setColor(string Color){
