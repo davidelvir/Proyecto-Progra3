580d68917f098a63933099186c20b803feb177c7
David <david_elvir@unitec.edu>
2017-06-03 17:14:57 -0600

    Ladron

---
+++ Ladron.cpp
@@ -0,0 +1,13 @@
+#include "Ladron.h"
+
+Ladron::Ladron(){
+
+  this->Vida=300;
+  this->Defensa=150;
+  this->Ataque=200;
+  this->Stamina=true;
+  this->Dinero=0;
+  this->Nivel=1;
+  this->Experiencia=0;
+  this->Bolas=0;
+}
---
+++ Ladron.h
@@ -0,0 +1,11 @@
+#include "Humano.h"
+#ifndef LADRON_H
+#define LADRON_H
+
+class Ladron : public Humano{
+
+  Ladron();
+  
+};
+
+#endif
